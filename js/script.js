24ce9133d4fdde6858a006fdc0eb2f45d55e3c41
kenny26ru <georgey26ru@gmail.com>
2020-03-02 23:08:45 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+        $('.gallery_slider').slick({
+          dots: true,
+          dotsClass: "dots",
+          infinite: true,
+          speed: 1500,
+          slidesToShow: 1,
+          centerMode: true,
+          variableWidth: true, 
+          prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-double-left"></i></button>',
+          nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-double-right"></i></button>',
+          responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                arrows: false
+              }
+            }
+          ]
+        });
+});
